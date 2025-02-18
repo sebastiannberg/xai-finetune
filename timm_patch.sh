889dc11b000f84a2182af70668e5f43acbd8f8f7
Sebastian August Berg <sebastian-august@hotmail.com>
2025-02-18 10:29:08 +0100

    Fix timm patch

--- timm_patch.sh
+++ timm_patch.sh
@@ -3,10 +3,6 @@
 timm_path_lib=/cluster/projects/uasc/sebastian/xai-finetune/venv/lib/python3.9/site-packages/timm
 timm_path_lib64=/cluster/projects/uasc/sebastian/xai-finetune/venv/lib64/python3.9/site-packages/timm
 
-# Ensure layers directory exists
-mkdir -p $timm_path_lib/layers
-mkdir -p $timm_path_lib64/layers
-
 # Copy to lib
 cp timm_patch/swin_transformer.py $timm_path_lib/models/
 cp timm_patch/helpers.py $timm_path_lib/models/layers/
