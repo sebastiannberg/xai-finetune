0e993e337c95f0d793d149afee118ae28d74ef51
Sebastian August Berg <sebastian-august@hotmail.com>
2025-02-20 00:33:53 +0100

    Fix timm patch paths

--- timm_patch.sh
+++ timm_patch.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-timm_path_lib=/cluster/projects/uasc/sebastian/xai-finetune/venv/lib/python3.9/site-packages/timm
-timm_path_lib64=/cluster/projects/uasc/sebastian/xai-finetune/venv/lib64/python3.9/site-packages/timm
+# Only change these two
+PYTHON_VERSION="3.10"
+VENV_NAME="venv"
+
+PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+VENV_PATH="${PROJECT_DIR}/${VENV_NAME}"
+TIMM_PATH_LIB="${VENV_PATH}/lib/python${PYTHON_VERSION}/site-packages/timm"
+TIMM_PATH_LIB64="${VENV_PATH}/lib64/python${PYTHON_VERSION}/site-packages/timm"
 
 # Copy to lib
-cp timm_patch/swin_transformer.py $timm_path_lib/models/
-cp timm_patch/helpers.py $timm_path_lib/models/layers/
+cp timm_patch/swin_transformer.py $TIMM_PATH_LIB/models/
+cp timm_patch/helpers.py $TIMM_PATH_LIB/models/layers/
 
 # Copy to lib64
-cp timm_patch/swin_transformer.py $timm_path_lib64/models/
-cp timm_patch/helpers.py $timm_path_lib64/models/layers/
+cp timm_patch/swin_transformer.py $TIMM_PATH_LIB64/models/
+cp timm_patch/helpers.py $TIMM_PATH_LIB64/models/layers/
 
