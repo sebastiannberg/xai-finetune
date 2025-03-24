9def7b23869025035b0d6e84339db28bf3cfaba3
Sebastian August Berg <sebastian-august@hotmail.com>
2025-03-24 13:20:31 +0100

    utils for plotting

---
+++ src/utils.py
@@ -0,0 +1,44 @@
+import numpy as np
+import torch
+import os
+from pathlib import Path
+import matplotlib.pyplot as plt
+
+
+PROJECT_ROOT = Path(__file__).parent.parent.absolute()
+
+def plot_class_attention_grads(class_attention_grads: torch.Tensor):
+    avg_class = class_attention_grads.mean(dim=0)
+    avg_block = avg_class.mean(dim=0)
+    avg_head = avg_block.mean(dim=0)
+    avg_query = avg_head.mean(dim=0)
+
+    plt.figure(figsize=(10, 6))
+    x_vals = np.arange(avg_query.size(0))
+    plt.plot(x_vals, avg_query.cpu().numpy())
+    plt.xlabel("Token Index")
+    plt.ylabel("Average Gradient Value")
+    plt.title("Average Attention Gradient (averaged over classes/blocks/heads/query)")
+    plt.tight_layout()
+
+    file_name = "class_attention_grads.png"
+    plt.savefig(os.path.join(PROJECT_ROOT, file_name))
+    plt.close()
+
+def plot_attention(attention: torch.Tensor):
+    avg_batch = attention.mean(dim=0)
+    avg_block = avg_batch.mean(dim=0)
+    avg_head = avg_block.mean(dim=0)
+    avg_query = avg_head.mean(dim=0)
+
+    plt.figure(figsize=(10, 6))
+    x_vals = np.arange(avg_query.size(0))
+    plt.plot(x_vals, avg_query.cpu().detach().numpy())
+    plt.xlabel("Token Index")
+    plt.ylabel("Average Attention Value")
+    plt.title("Average Attention (averaged over batch/blocks/heads/query)")
+    plt.tight_layout()
+
+    file_name = "attention.png"
+    plt.savefig(os.path.join(PROJECT_ROOT, file_name))
+    plt.close()
